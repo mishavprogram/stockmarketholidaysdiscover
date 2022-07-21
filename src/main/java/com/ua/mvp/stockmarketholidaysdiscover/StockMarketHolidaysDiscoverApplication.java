ed5d10baa3f11de8efa5bd9f5a7be22efddd2052
Mykhailo <mishavprogram@ukr.net>
2022-07-21 13:11:55 +0300

    init commit

---
+++ src/main/java/com/ua/mvp/stockmarketholidaysdiscover/StockMarketHolidaysDiscoverApplication.java
@@ -0,0 +1,13 @@
+package com.ua.mvp.stockmarketholidaysdiscover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketHolidaysDiscoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketHolidaysDiscoverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ua/mvp/stockmarketholidaysdiscover/StockMarketHolidaysDiscoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.mvp.stockmarketholidaysdiscover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketHolidaysDiscoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
