294cb4fde74ca65dc59a5df4635da4eb89a31ee6
Mykhailo <mishavprogram@ukr.net>
2022-07-21 21:39:48 +0300

    added simple exception handler

---
+++ src/main/java/com/ua/mvp/stockmarketholidaysdiscover/exceptions/ExceptionMessages.java
@@ -0,0 +1,6 @@
+package com.ua.mvp.stockmarketholidaysdiscover.exceptions;
+
+public class ExceptionMessages {
+    public static final String IO_EXCEPTION_MESSAGE = "Open or edit file (csv) finished with exception";
+    public static final String GENERIC_EXCEPTION_MESSAGE = "unexpected generic exception";
+}
---
+++ src/main/java/com/ua/mvp/stockmarketholidaysdiscover/exceptions/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.ua.mvp.stockmarketholidaysdiscover.exceptions;
+
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.io.UncheckedIOException;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {UncheckedIOException.class})
+    protected ResponseEntity<Object> handleIOConflict(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = ExceptionMessages.IO_EXCEPTION_MESSAGE;
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+
+    @ExceptionHandler(value = {RuntimeException.class})
+    protected ResponseEntity<Object> handleGenericConflict(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = ExceptionMessages.GENERIC_EXCEPTION_MESSAGE;
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+}
